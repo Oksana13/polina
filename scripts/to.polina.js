aedece16214610392aa629975894a80f4d2c261e
Oksana <klimovaoxana.v@gmail.com>
2016-11-08 11:09:51 +0300

    update script to send content

--- scripts/to.polina.js
+++ scripts/to.polina.js
@@ -1,9 +1,9 @@
-var name = $('#name').val(),
-    email = $('#email').val(),
-    app = $('#editable').text(),
-    description = app + '<br><br>' + name + '<br>' + email;
-
 function send() {
+    var name = $('#name').val(),
+        email = $('#email').val(),
+        app = $('#editable').text(),
+        description = app + '<br><br>' + name + '<br>' + email;
+
     $.post('http://polina.crisiscenter.ru/wp-admin/admin-ajax.php', {
         action: 'external_add_task',
         status: 'publish',
