52b3d327f3b83d1a63c30ab611d4666088e7b3bf
Oksana <klimovaoxana.v@gmail.com>
2016-11-22 03:12:47 +0300

    Fix sending data after form validation

--- scripts/to.polina.js
+++ scripts/to.polina.js
@@ -1,35 +1,45 @@
-$('#send').click(function (){
+$('#send').click(function (e){
     var name = $('#name').val(),
         email = $('#email').val(),
         app = $('#editable').text(),
         description = app + '<br><br>' + name + '<br>' + email;
 
-    $('form.info').submit(function (e) {
 
-        $.post(
-            'http://polina.crisiscenter.ru/wp-admin/admin-ajax.php',
-            {
-                action: 'external_add_task',
-                status: 'publish',
-                title: 'Новое дело',
-                descr: description
-            })
-            .success(onAjaxSuccess)
-            .error(onAjaxError);
+    if( name.length >= 1 && email.length >= 1 ) {
+        $('form.info').submit(function (e) {
+            $.post(
+                'http://polina.crisiscenter.ru/wp-admin/admin-ajax.php',
+                {
+                    action: 'external_add_task',
+                    status: 'publish',
+                    title: 'Новое дело',
+                    descr: description
+                })
+                .success(onAjaxSuccess)
+                .error(onAjaxError);
 
-        e.preventDefault();
+            e.preventDefault();
 
-        function onAjaxSuccess() {
-            $('#success').fadeIn().delay(5000).fadeOut();
-        }
+            function onAjaxSuccess() {
+                $('#success').fadeIn().delay(3000).fadeOut();
+                $('#fields').hide();
+            }
 
-        function onAjaxError() {
-            $('#error').fadeIn().delay(5000).fadeOut();
-        }
+            function onAjaxError() {
+                $('#error').fadeIn().delay(3000).fadeOut();
+                $('#fields').hide();
+            }
+        });
 
-    });
+        } else {
+            $('#fields').fadeIn().delay(3000).fadeOut();
+            e.preventDefault();
 
+    }
 
+});
 
+$('.close').click(function () {
+    $('.alert').hide();
 });
 
